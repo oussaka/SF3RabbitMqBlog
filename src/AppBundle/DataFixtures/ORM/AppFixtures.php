02c660df61687ac21fd7321013652c73892ffb74
oussaka <oussaka@users.noreply.github.com>
2017-08-16 20:42:15 +0000

    Custom Alice Faker Function

--- src/AppBundle/DataFixtures/ORM/AppFixtures.php
+++ src/AppBundle/DataFixtures/ORM/AppFixtures.php
@@ -7,12 +7,46 @@ use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Nelmio\Alice\Fixtures;
 
+
+/**
+ * Class LoadFixtures
+ * tutoriel https://knpuniversity.com/screencast/symfony-doctrine/alice-faker-function#play
+ *
+ * @package AppBundle\DataFixtures\ORM
+ */
 class LoadFixtures implements FixtureInterface
 {
 
     public function load(ObjectManager $manager)
     {
-        $objects = Fixtures::load(__DIR__ . '/blog_posts.yml', $manager);
+        $objects = Fixtures::load(
+            __DIR__.'/blog_posts.yml',
+            $manager,
+            [
+                'providers' => [$this]
+            ]
+        );
+    }
+    public function title()
+    {
+        $genera = [
+            'Octopus',
+            'Balaena',
+            'Orcinus',
+            'Hippocampus',
+            'Asterias',
+            'Amphiprion',
+            'Carcharodon',
+            'Aurelia',
+            'Cucumaria',
+            'Balistoides',
+            'Paralithodes',
+            'Chelonia',
+            'Trichechus',
+            'Eumetopias'
+        ];
+        $key = array_rand($genera);
+        return $genera[$key];
     }
 
 }
